7388ea634f2bd2e648de2bffec609a6233ce03f2
Dirk <dirk.zarth@axa.com>
2019-11-21 08:32:20 +0000

    function is broken

--- modules/demo/lib/puppet/functions/mastername.rb
+++ modules/demo/lib/puppet/functions/mastername.rb
@@ -1,14 +1,17 @@
 #mastername.rb
-
+ 
 Puppet::Functions.create_function(:mastername) do
   dispatch :mastername do
-    #param 'String', :filename
-    #optional_param 'String', :foo
-    #return_type 'String'
+    param 'String', :filename
+    optional_param 'String', :foo
+    return_type 'String'
   end
-
-  def mastername
+ 
+  def mastername(*arguments)
     #puts filename.to_s
-    File.open(/etc/puppetlabs/puppet/puppet.conf).grep(/server =/).join.split(' ').slice(2)
+    File.open('/etc/puppetlabs/puppet/puppet.conf').grep(/server\s*=/).join.split(' ').slice(2)
+    # Below line works as welll. Where \= is escaped
+    #File.open('/etc/puppetlabs/puppet/puppet.conf').grep(/server\s*\=/).join.split(' ').slice(2)
   end
 end
+
